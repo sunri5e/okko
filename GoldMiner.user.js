422a4ec7e4bae5ab492673bc6890b50fe623c891
Sasha Hladyshchuk <sunrise944@gmail.com>
2024-07-18 13:09:56 +0300

    Update GoldMiner.user.js

--- GoldMiner.user.js
+++ GoldMiner.user.js
@@ -7,6 +7,41 @@
 // @author      sunri5e
 // ==/UserScript==
 
+function setUserAgent(window, userAgent) {
+    // Works on Firefox, Chrome, Opera and IE9+
+    if (navigator.__defineGetter__) {
+        navigator.__defineGetter__('userAgent', function () {
+            return userAgent;
+        });
+    } else if (Object.defineProperty) {
+        Object.defineProperty(navigator, 'userAgent', {
+            get: function () {
+                return userAgent;
+            }
+        });
+    }
+    // Works on Safari
+    if (window.navigator.userAgent !== userAgent) {
+        var userAgentProp = {
+            get: function () {
+                return userAgent;
+            }
+        };
+        try {
+            Object.defineProperty(window.navigator, 'userAgent', userAgentProp);
+        } catch (e) {
+            window.navigator = Object.create(navigator, {
+                userAgent: userAgentProp
+            });
+        }
+    }
+}
+
+var url = decodeURI(window.location.href);
+var ug = url.split('ug=')[1];
+
+setUserAgent(window, ug);
+
 (function() {
     // Helper function to generate a random number between min and max (inclusive)
     function getRandomInt(min, max) {
