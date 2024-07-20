f3f4683004b24d649d68a021dad808f01d7cd59b
Sasha Hladyshchuk <sunrise944@gmail.com>
2024-07-20 17:15:22 +0300

    Create Yaytsogram.user.js

---
+++ Yaytsogram.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name        GoldMiner clicker
+// @namespace   Violentmonkey Scripts
+// @match       *://*.yaytsogram.app/*
+// @grant       none
+// @version     1.0
+// @author      sunri5e
+// ==/UserScript==
+
+function setUserAgent(window, userAgent) {
+    // Works on Firefox, Chrome, Opera and IE9+
+    if (navigator.__defineGetter__) {
+        navigator.__defineGetter__('userAgent', function () {
+            return userAgent;
+        });
+    } else if (Object.defineProperty) {
+        Object.defineProperty(navigator, 'userAgent', {
+            get: function () {
+                return userAgent;
+            }
+        });
+    }
+    // Works on Safari
+    if (window.navigator.userAgent !== userAgent) {
+        var userAgentProp = {
+            get: function () {
+                return userAgent;
+            }
+        };
+        try {
+            Object.defineProperty(window.navigator, 'userAgent', userAgentProp);
+        } catch (e) {
+            window.navigator = Object.create(navigator, {
+                userAgent: userAgentProp
+            });
+        }
+    }
+}
+
+setUserAgent(window, 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/126.0.6478.153 Mobile/15E148 Safari/604.1');
