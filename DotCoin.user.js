66808292e23fcb83cf54f06593db7548012b91e1
Sasha Hladyshchuk <sunrise944@gmail.com>
2024-07-26 14:39:14 +0300

    Update DotCoin.user.js

--- DotCoin.user.js
+++ DotCoin.user.js
@@ -2,16 +2,22 @@
 // @name        DotCoin clicker
 // @namespace   Violentmonkey Scripts
 // @match       https://dot.dapplab.xyz/*
+// @match       https://app.dotcoin.bot/*
 // @grant       none
 // @version     1.0
 // @author      sunri5e
 // ==/UserScript==
 
-function getRandomMs(base, max) {
-    return base + Math.floor(Math.random() * max);
-}
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
-function tap(element) {
+(async () => {
+  await delay(5000);
+
+  function getRandomMs(base, max) {
+    return base + Math.floor(Math.random() * max);
+  }
+
+  function simulateTouchStart(element) {
     if (!element) {
         console.error('Element is required');
         return;
@@ -40,80 +46,91 @@ function tap(element) {
     });
 
     element.dispatchEvent(touchEvent);
-}
+  }
 
-function startTapping() {
+  function startTapping() {
+    console.log('Starting tapping...');
     const clickerCoin = document.querySelector('.ClickerCoin');
     const clickerCoinDot = document.querySelector('.ClickerCoinDot');
     const scoreBarInfoLimit = document.querySelector('.ScoreBarInfoLimit');
+    const progressBar = document.querySelector('.ProgressBarFill');
 
     if (!clickerCoin || !clickerCoinDot || !scoreBarInfoLimit) {
         console.error('Required elements not found!');
+        document.body.appendChild(finishBanner);
         return;
     }
 
-    let countdown = 25;
-    let tappingInterval;
-    let tappingConditionInterval;
+    function startTapSimulation(element, initialFrequency) {
+      let currentFrequency = initialFrequency;
+      let tapInterval;
 
-    function startCountdown() {
-        const countdownInterval = setInterval(() => {
-            countdown--;
-            if (countdown <= 0) {
-                clearInterval(countdownInterval);
-                clearInterval(tappingConditionInterval);
-                clearInterval(tappingInterval);
-                tapFrequently();
-            }
-        }, 1000);
-    }
+      function adjustFrequency() {
+          const elementWidth = element.getBoundingClientRect().width;
+          const progressBarWidth = progressBar.getBoundingClientRect().width;
+          const windowWidth = window.innerWidth;
+          console.log(`Element width: ${elementWidth}, Progress bar width: ${progressBarWidth/windowWidth}`);
 
-    function tapFrequently() {
-        tappingInterval = setInterval(() => {
-            tap(clickerCoin);
-        }, getRandomMs(200, 100));
-    }
+          if (elementWidth > 180 && progressBarWidth/windowWidth < 0.15) {
+            clearInterval(tapInterval);
+            currentFrequency = getRandomMs(100, 100);
+            tapInterval = setInterval(tap, currentFrequency);
+            console.log('Tapping more frequently');
+          } else if (elementWidth > 180) {
+              clearInterval(tapInterval);
+              currentFrequency = getRandomMs(300, 200);
+              tapInterval = setInterval(tap, currentFrequency);
+              console.log('Tapping less frequently');
+          } else if (elementWidth <= 180) {
+              clearInterval(tapInterval);
+              currentFrequency = getRandomMs(100, 100);
+              tapInterval = setInterval(tap, currentFrequency);
+              console.log('Tapping more frequently');
+          }
+      }
 
-    function tapWithCondition() {
-        tappingConditionInterval = setInterval(() => {
-            const width = parseInt(clickerCoinDot.style.width);
-            if (width >= 180 && width < 200) {
-                tap(clickerCoin);
-                clearInterval(tappingInterval);
-                tappingInterval = setInterval(() => {
-                    tap(clickerCoin);
-                }, getRandomMs(500, 200));
-            } else if (width < 180) {
-                clearInterval(tappingConditionInterval);
-                tapFrequently();
-            }
-        }, 200);
-    }
-
-    function startCycle() {
-        if (parseInt(scoreBarInfoLimit.textContent) > 0) {
-            setTimeout(() => {
-                tapFrequently();
-                startCountdown();
-                tapWithCondition();
-            }, 3000);
+      function tap() {
+        if (scoreBarInfoLimit && scoreBarInfoLimit.textContent.trim() === '0') {
+            clearInterval(tapInterval); // Stop the interval
+            console.log('Stopping script as the value is 0');
+            document.body.appendChild(finishBanner);
+            return; // Stop execution
         }
+
+        simulateTouchStart(element);
+        adjustFrequency();
+      }
+
+      tapInterval = setInterval(tap, currentFrequency);
     }
 
-    startCycle();
-}
+    startTapSimulation(clickerCoinDot, 2000);
+  }
+  
+  const finishBanner = document.createElement('div');
+  finishBanner.className = 'finish-banner';
+  finishBanner.textContent = 'Window can be closed now';
 
-const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-        if (mutation.addedNodes.length) {
-            mutation.addedNodes.forEach((node) => {
-                if (node.classList && node.classList.contains('PageMain')) {
-                    observer.disconnect();
-                    startTapping();
-                }
-            });
-        }
-    });
-});
+  const style = document.createElement('style');
+  style.textContent = `
+    .finish-banner {
+      position: fixed;
+      bottom: 150px;
+      left: 20px;
+      background-color: rgba(36, 146, 255, 0.8);
+      color: #fff;
+      border: none;
+      border-radius: 20px;
+      width: auto;
+      height: 40px;
+      font-size: 18px;
+      cursor: pointer;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+      z-index: 9999;
+      padding: 6px 20px;
+    }
+  `;
+  document.head.appendChild(style);
 
-observer.observe(document.body, { childList: true, subtree: true });
+  startTapping();
+})();
